195f4d25bdf30f355d029697675481ba3193ad77
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-03-08 19:00:35 +0100

    Fix testing for wrong platform name

--- gatt/gatt.py
+++ gatt/gatt.py
@@ -1,7 +1,7 @@
 import os
 import platform
 
-if platform.system() == 'Darwin':
+if platform.system() == 'Linux':
     if os.environ.get('LINUX_WITHOUT_DBUS', '0') == '0':
         from .gatt_linux import *
     else:
