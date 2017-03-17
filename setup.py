b589f479891175938d86e9588939f508d107bf49
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-03-17 13:49:49 +0100

    Set version to 0.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='gatt',
     packages=['gatt'],
-    version='0.2.1',
+    version='0.2.2',
     description='Bluetooth GATT SDK for Python',
     keywords='gatt',
     url='https://github.com/getsenic/gatt-python',
